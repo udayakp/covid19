40505da1625eff76b31ad0d1e97f3a2a3d07e0ec
Udaya Krishnappa Prakash <Udaya_Prakash@ad.infosys.com>
2020-05-09 15:02:43 +0530

    First commit

---
+++ app_flutter/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:app_flutter/ui/screens/homescreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        primarySwatch: Colors.blue,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        //visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Item Finder'),
+    );
+  }
+}
+
